d1cfe37ba45ddf878fa1ca1c89cf75b18fec7025
TheOarsman <heinzulrichv@gmail.com>
2024-03-02 12:25:10 -0500

    Adding Necessary Components

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import 'App.css'
+import PlaceHolder from '.components/PlaceHolder';
+
+function App() {
+    return (
+        <div className="placeHolder-app">
+            <PlaceHolder />
+        </div>
+    );
+}
+
+export default App;
