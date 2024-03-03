95c3a81d0a9d44b18d9310a52922583f2fb56486
TheOarsman <heinzulrichv@gmail.com>
2024-03-03 13:04:17 -0500

    Footer icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-// import { VscGithubAlt } from "react-icons/vsc";
-// import { AiOutlineLinkedin } from "react-icons/ai";
-// import { FaInstagram } from "react-icons/fa";
+import { GitHubFooterIMG } from "../../assets/images/footer/githubfooter.png";
+import { LinkedInFooterIMG } from "../../assets/images/footer/linkedinfooter.png";
+import { YouTubeFooterIMG } from "../../assets/images/footer/youtubefooter.png";
+import { GmailFooterIMG } from "../../assets/images/footer/gmailfooter.png";
 
 function Footer() {
   return (
@@ -11,42 +12,24 @@ function Footer() {
         <ul>
           <li>
             <a href="https://www.github.com/TheOarsman">
-              <img src="assets/images/footer/githubfooter.png" alt="GitHub" />
+              <GitHubFooterIMG />
             </a>
           </li>
           <li>
             <a href="https://www.linkedin.com/in/heinz-ulrich-v-3a3486a0/">
-              <img
-                src="assets/images/footer/linkedinfooter.png"
-                alt="LinkedIn"
-              />
+              <LinkedInFooterIMG />
             </a>
           </li>
           <li>
             <a href="https://www.youtube.com/@theoarsman4581">
-              <img src="assets/images/footer/youtubefooter.png" alt="YouTube" />
+              <YouTubeFooterIMG />
             </a>
           </li>
           <li>
             <a href="mailto:heinzulrichv@gmail.com">
-              <img src="assets/images/footer/gmailfooter.png" alt="Gmail" />
+              <GmailFooterIMG />
             </a>
           </li>
-          {/* <a href="https://github.com/TheOarsman">
-            <li className="logo">
-              <VscGithubAlt />
-            </li>
-          </a>
-          <a href="https://www.linkedin.com/in/lindsey-m-2219abb0/">
-            <li className="logo">
-              <AiOutlineLinkedin />
-            </li>
-          </a>
-          <a href="https://www.instagram.com/lem_photos/">
-            <li className="logo">
-              <FaInstagram />
-            </li>
-          </a> */}
         </ul>
       </p>
     </footer>
