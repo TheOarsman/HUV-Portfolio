5776de49955047724711d9ab2ac79fe2df008cb8
TheOarsman <heinzulrichv@gmail.com>
2024-03-03 23:31:58 -0500

    img path for photo corrected

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import profileImage from "../../assets/images/others/heinz.JPG";
+import profileImage from "/Users/huv/Documents/Coding/HUV-Portfolio/src/assets/images/others/heinz.jpg";
 function About() {
   return (
     <section id="about-me" className="my-5 intro">
