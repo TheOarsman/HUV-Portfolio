25bcc7464edc09d3d88a209863ee0b49938ff87e
TheOarsman <heinzulrichv@gmail.com>
2024-03-06 21:38:15 -0500

    styling added, and footer with icons fixed

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,30 +8,42 @@ function Footer() {
   return (
     <footer>
       <h2>Made by Heinz Ulrich V©</h2>
-      <p>
-        <ul>
-          <li>
-            <a href="https://www.github.com/TheOarsman">
-              <img src={gitHubFooterIMG} />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/heinz-ulrich-v-3a3486a0/">
-              <img src={linkedInFooterIMG} />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.youtube.com/@theoarsman4581">
-              <img src={youTubeFooterIMG} />
-            </a>
-          </li>
-          <li>
-            <a href="mailto:heinzulrichv@gmail.com">
-              <img src={gmailFooterIMG} />
-            </a>
-          </li>
-        </ul>
-      </p>
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        <li
+          className="logo"
+          style={{ display: "inline-block", marginRight: "10px" }}
+        >
+          <a href="https://www.github.com/TheOarsman">
+            <img src={gitHubFooterIMG} alt="GitHub" height="24" width="24" />
+          </a>
+        </li>
+        <li
+          className="logo"
+          style={{ display: "inline-block", marginRight: "10px" }}
+        >
+          <a href="https://www.linkedin.com/in/heinz-ulrich-v-3a3486a0/">
+            <img
+              src={linkedInFooterIMG}
+              alt="LinkedIn"
+              height="24"
+              width="24"
+            />
+          </a>
+        </li>
+        <li
+          className="logo"
+          style={{ display: "inline-block", marginRight: "10px" }}
+        >
+          <a href="https://www.youtube.com/@theoarsman4581">
+            <img src={youTubeFooterIMG} alt="YouTube" height="24" width="24" />
+          </a>
+        </li>
+        <li className="logo" style={{ display: "inline-block" }}>
+          <a href="mailto:heinzulrichv@gmail.com">
+            <img src={gmailFooterIMG} alt="Gmail" height="24" width="24" />
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 }
