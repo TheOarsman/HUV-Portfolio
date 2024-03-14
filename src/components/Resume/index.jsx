6efa1f15421839826c20093d0cfdcc915cb3e731
TheOarsman <heinzulrichv@gmail.com>
2024-03-13 21:11:10 -0400

    Resume Colum Titles & attributes updated

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -36,18 +36,24 @@ function Resume() {
       {/* // This is the resume language */}
       <section id="home-page-body" className="resume-body">
         <div className="article column1">
-          <p className="column-title">Font-End Tech</p>
+          <p className="column-title">Font-End Development</p>
           <p className="column-text">
             <ul>
               <li className="logo">
                 <MdFoundation />
               </li>
-              <li>HTML5</li>
+              <li>HTML</li>
               <li>CSS</li>
-              <li>JavaScript</li>
-              <li>APIs</li>
               <li>Bootstrap</li>
-              <li>GIT</li>
+              <li>Font Awesome</li>
+              <li>JavaScript</li>
+              <li>jQuery</li>
+              <li>JSX</li>
+              <li>React</li>
+              <li>React Hooks</li>
+              <li>SCG</li>
+              <li>Day.js</li>
+              <li>Workbox</li>
             </ul>
           </p>
         </div>
@@ -55,38 +61,93 @@ function Resume() {
         <div className="article column2">
           {/* <p><img src="./assets/images/streamline-icon-ai-robot-waiter@48x48.PNG"></p> */}
 
-          <p className="column-title">Back-End Tech</p>
+          <p className="column-title">Back-End Development</p>
           <p className="column-text">
             <ul>
               <li className="logo">
                 <GrTechnology />
               </li>
               <li>Node.Js</li>
-              <li>Jest</li>
-              <li>Express.js</li>
-              <li>MySQL</li>
+              <li>Express</li>
+              <li>MVC</li>
+              <li>Controllers</li>
+              <li>Models</li>
+              <li>Routes</li>
               <li>Sequelize</li>
-              <li>Object-Relational Mapping(ORM)</li>
-              <li>Model-View-Controller (MVC)</li>
+              <li>dotenv</li>
+              <li>mysql2</li>
+              <li>MongoDB</li>
+              <li>Shell Script</li>
+              <li>Connection.js</li>
+              <li>Object-Relational Mapping</li>
+              <li>JSON</li>
             </ul>
           </p>
         </div>
         <div className="article column3">
           {/* <p><img src="./assets/images/streamline-icon-soft-drinks-glass-ice@48x48.PNG"></p> */}
-          <p className="column-title">Performance Tech</p>
+          <p className="column-title">Development Tools & Utilities</p>
           <p className="column-text">
             <ul>
               <li className="logo">
                 <GrDocumentPerformance />
               </li>
-              <li>NoSQL</li>
-              <li>Progressive Web Applications (PWA)</li>
+              <li>Babel Configuration</li>
+              <li>Jest</li>
+              <li>Nodemon</li>
+              <li>Git</li>
+              <li>Package.json</li>
+              <li>webpack Configuration</li>
+              <li>Vite Configuration</li>
+              <li>dotenv</li>
+              <li>Index.js</li>
+            </ul>
+          </p>
+        </div>
+      </section>
+      <section id="home-page-body" className="resume-body">
+        <div className="article column1">
+          <p className="column-title">Data Storage & Management</p>
+          <p className="column-text">
+            <ul>
+              <li className="logo">
+                <MdFoundation />
+              </li>
+              <li>MySQL</li>
               <li>MongoDB</li>
-              <li>Mongoose</li>
-              <li>GraphQL</li>
-              <li>React</li>
-              <li>MERN</li>
-              <li>State</li>
+              <li>Sequelize</li>
+              <li>Local Storage</li>
+            </ul>
+          </p>
+        </div>
+
+        <div className="article column2">
+          {/* <p><img src="./assets/images/streamline-icon-ai-robot-waiter@48x48.PNG"></p> */}
+
+          <p className="column-title">APIs & Integration</p>
+          <p className="column-text">
+            <ul>
+              <li className="logo">
+                <GrTechnology />
+              </li>
+              <li>API Integration</li>
+              <li>JSON</li>
+            </ul>
+          </p>
+        </div>
+        <div className="article column3">
+          {/* <p><img src="./assets/images/streamline-icon-soft-drinks-glass-ice@48x48.PNG"></p> */}
+          <p className="column-title">Miscellaneous</p>
+          <p className="column-text">
+            <ul>
+              <li className="logo">
+                <GrDocumentPerformance />
+              </li>
+              <li>ASCII art</li>
+              <li>Fs</li>
+              <li>Inquirer</li>
+              <li>Markdown</li>
+              <li>Environmental Variables</li>
             </ul>
           </p>
         </div>
