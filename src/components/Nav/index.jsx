6b88f39d99850bc9b240afb517cf0780db19db33
TheOarsman <heinzulrichv@gmail.com>
2024-03-19 18:29:05 -0400

    Trying to set up Link components

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
+import { Link } from "react-router-dom"; // Import Link component from React Router
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
-function Nav(props) {
-  const { currentTab, handleTabChange } = props;
-
+function Nav({ currentTab, handleTabChange }) {
   useEffect(() => {
     document.title = capitalizeFirstLetter(currentTab.name);
   }, [currentTab]);
@@ -14,17 +13,17 @@ function Nav(props) {
     <nav>
       <ul className="nav nav-tabs">
         <li className="nav-item">
-          <a
+          <Link
             href="#about"
             onClick={() => handleTabChange("About")}
             // Check to see if the currentTab is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentTab === "About" ? "nav-link active" : "nav-link"}
           >
             About
-          </a>
+          </Link>
         </li>
         <li className="nav-item">
-          <a
+          <Link
             href="#portfolio"
             onClick={() => handleTabChange("Portfolio")}
             // Check to see if the currentTab is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
@@ -33,10 +32,10 @@ function Nav(props) {
             }
           >
             Portfolio
-          </a>
+          </Link>
         </li>
         <li className="nav-item">
-          <a
+          <Link
             href="#contact"
             onClick={() => handleTabChange("Contact")}
             // Check to see if the currentTab is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
@@ -45,17 +44,17 @@ function Nav(props) {
             }
           >
             Contact
-          </a>
+          </Link>
         </li>
         <li className="nav-item">
-          <a
+          <Link
             href="#resume"
             onClick={() => handleTabChange("Resume")}
             // Check to see if the currentTab is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentTab === "Resume" ? "nav-link active" : "nav-link"}
           >
             Resume
-          </a>
+          </Link>
         </li>
       </ul>
     </nav>
