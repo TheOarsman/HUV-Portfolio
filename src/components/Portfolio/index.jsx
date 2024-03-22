c84baa8a61f0b676a2ba6d37dfee0917c88468d9
TheOarsman <heinzulrichv@gmail.com>
2024-03-21 23:57:21 -0400

    update portfolio for proper react function

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -35,8 +35,149 @@ function Portfolio() {
                 plan their business effectively."
           repoURL="https://github.com/TheOarsman/SQL_Employee_Tracker"
         ></Project>
+        <Project
+          deployedLink="https://youtu.be/XMI2PqBvzY8"
+          thumbnail={eCommerce}
+          demoType="video"
+          name="E-Commerce: Back-End ORM"
+          description="This demonstration illustrates the fundamental architecture of
+                an e-commerce site. The backend was configured with a functional
+                Express.js API, seamlessly integrated with Sequelize to interact
+                with a MySQL database. This setup forms the backbone of the
+                e-commerce platform, enabling smooth data handling and efficient
+                communication between the frontend and backend components."
+          repoURL="https://github.com/TheOarsman/ORM-E-Commerce-Back-End"
+        ></Project>
+        <Project
+          deployedLink="https://youtu.be/Oqe-QMF2NqM"
+          thumbnail={readMeGen}
+          demoType="video"
+          name="README Generator"
+          description="This command-line app swiftly creates professional README.md
+                files using the Inquirer Package. It's designed for developers
+                who need to generate READMEs quickly and efficiently, enabling
+                them to create professional documentation for their projects
+                with ease."
+          repoURL="https://github.com/TheOarsman/README-Generator"
+        ></Project>
+        <Project
+          deployedLink="https://youtu.be/7Mlw5yssC3w"
+          thumbnail={socialNetwork}
+          demoType="video"
+          name="Social Network"
+          description="This NoSQL Social Network API application provides
+                functionalities for users to interact within a social network.
+                It enables users to share thoughts, react to friends' thoughts,
+                and manage friend lists. Developed with Express.js for routing
+                and MongoDB for the database, it utilizes Mongoose ODM for
+                seamless interaction and schema-based modeling in Node.js."
+          repoURL="https://github.com/TheOarsman/Social-Network-API"
+        ></Project>
+        <Project
+          deployedLink="https://youtu.be/evxzpTv3JEE"
+          thumbnail={svgLogoMaker}
+          demoType="video"
+          name="SVG Logo Maker"
+          description="The SVG Logo Maker is a Node.js command-line tool for freelance
+                web developers, enabling quick generation of simple logos
+                without graphic design expertise. Built with Object-oriented
+                Programming, it offers customization options for text, color,
+                and shape selection. With Jest for testing and Inquirer for user
+                interaction, it ensures accuracy and a smooth experience,
+                producing an SVG file named logo.svg upon completion."
+          repoURL="https://github.com/TheOarsman/SVG-Logo-Maker"
+        ></Project>
+        <Project
+          deployedLink="https://theoarsman.github.io/Who-Is-The-Heinz-Barron-/"
+          thumbnail={heinzBarronPortfolio}
+          demoType="deployed"
+          name="The Heinz Barron's 1st Portfolio"
+          description="Five months ago, I took my first steps into web development with
+                this initial portfolio. It was a journey of discovery, filled
+                with challenges and learning. Today, I stand before you with a
+                new and updated portfolio, showcasing the remarkable progress
+                I've made, thanks to dedication and continuous learning. Dive in
+                and witness the evolution firsthand!"
+          repoURL="https://github.com/TheOarsman/Who-Is-The-Heinz-Barron-"
+        ></Project>
+        <Project
+          deployedLink="https://desolate-sierra-47851-d89056b73f92.herokuapp.com/"
+          thumbnail={sillyAPI}
+          demoType="deployed"
+          name="Slappi API"
+          description=" A website for all things wonky and silly, that coders and
+                programmers can have fun!"
+          repoURL="https://github.com/TheOarsman/slAPI"
+        ></Project>
+        <Project
+          deployedLink="https://theoarsman.github.io/Weather_Dashboard_SS_API/"
+          thumbnail={weatherDash1}
+          demoType="deployed"
+          name="Weather Dashboard"
+          description="This browser-based weather dashboard offers developers seamless
+                access to weather data for cities through third-party APIs. By
+                harnessing the 5 Day Weather Forecast, it dynamically updates
+                HTML and CSS to deliver precise and current weather information
+                to users. Moreover, it employs localStorage for persistent data
+                storage, ensuring that user preferences and settings remain
+                intact across sessions."
+          repoURL="https://github.com/TheOarsman/Weather_Dashboard_SS_API"
+        ></Project>
+        <Project
+          deployedLink="https://jate-just-another-text-editor-ql4f.onrender.com/"
+          thumbnail={textEditor}
+          demoType="deployed"
+          name="J.A.T.E. - Just Another Text Editor"
+          description="J.A.T.E is a dynamic text editor designed for developers and
+                users seeking versatile editing capabilities. With offline
+                functionality as a Progressive Web App, robust data persistence,
+                and an intuitive interface, it offers a seamless editing
+                experience. Deployed on Render for reliability and scalability,
+                J.A.T.E ensures uninterrupted access to your content, anytime,
+                anywhere."
+          repoURL="https://github.com/TheOarsman/PWA-Text-Editor"
+        ></Project>
+        <Project
+          deployedLink="https://theoarsman.github.io/"
+          thumbnail={passWordGen}
+          demoType="deployed"
+          name="Password Generator"
+          description="This page showcases a Random Password Generator. The generator
+                prompts users to specify their preferences for password
+                criteria, including length, special characters, upper/lower case
+                letters, and numerics. Based on these selections, the tool
+                generates a random password tailored to the user's
+                specifications."
+          repoURL="https://github.com/TheOarsman/Rodnam-Proswasd-Gnereotar"
+        ></Project>
+        <Project
+          deployedLink="https://theoarsman.github.io/Work-Day-Scheduler/"
+          thumbnail={workDayScheduler}
+          demoType="deployed"
+          name="Workday Scheduler"
+          description="This is a simple calendar application tailored for employees to
+                manage their daily schedules effectively. The application allows
+                users to save events for each hour of a typical workday
+                (9AM-5PM) by modifying starter code. It leverages the Day.js
+                library to handle date and time functionalities. The user story
+                entails the need to add important events to a daily planner for
+                efficient time management."
+          repoURL="https://github.com/TheOarsman/Work-Day-Scheduler"
+        ></Project>
+        <Project
+          deployedLink="https://memosphere-a4428ea90189.herokuapp.com/"
+          thumbnail={noteTaker}
+          demoType="deployed"
+          name="Note Taker"
+          description="The Note Taker is a user-friendly web app enabling note
+                creation, saving, and deletion. Ideal for organizing thoughts,
+                managing tasks, and storing essential information, it offers
+                intuitive features such as note creation, saving, viewing, and
+                deletion."
+          repoURL="https://github.com/TheOarsman/Express.js_Note_Taker"
+        ></Project>
 
-        <div className="job">
+        {/* <div className="job">
           <div className="job-info">
             <div className="job-img">
               <a href="https://youtu.be/XMI2PqBvzY8" target="_blank">
@@ -184,9 +325,6 @@ function Portfolio() {
             </div>
           </div>
         </div>
-
-        {/* Website Portion of Porfolio */}
-
         <div className="job">
           <div className="job-info">
             <div className="job-img">
@@ -464,7 +602,7 @@ function Portfolio() {
               </a>
             </div>
           </div>
-        </div>
+        </div> */}
       </section>
     </div>
   );
